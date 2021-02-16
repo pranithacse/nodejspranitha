bb17c76a20411e719456c366049caa24161dcbb6
Pranitha Rallabandi <40241348+pranithacse@users.noreply.github.com>
2021-02-16 10:42:16 +0530

    console.js

---
+++ console.js
@@ -0,0 +1,28 @@
+// program to write to console
+
+// passing number 
+console.log(8);
+
+// passing string
+console.log('hello');
+
+// passing variable
+const x = 'hello';
+console.log(x);
+
+// passing function
+function sayName() {
+    return 'Hello Pranitha';
+}
+console.log(sayName());
+
+// passing string and a variable
+const name = 'Pranitha';
+console.log('Hello ' + name);
+
+// passing object
+let obj = {
+    name: 'Pranitha',
+    age: 20
+}
+console.log(obj);
